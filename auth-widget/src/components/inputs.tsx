4999432411a7a5f38719008e0ae13df71ca46de7
Jack Young <jdydev2003@gmail.com>
2025-12-01 10:45:30 -0800

    separate inputs file

---
+++ auth-widget/src/components/inputs.tsx
@@ -0,0 +1,29 @@
+import styled from "styled-components";
+
+const InputContainer = styled.div`
+  width: 81.5%;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+const Input = styled.input<{ $styleString: string }>`
+  ${(props) => props.$styleString}
+
+  &::placeholder {
+    color: #64748b;
+    opacity: 1;
+  }
+
+  &:focus {
+    outline: 2px solid #3b82f6;
+    outline-offset: 0;
+    border-color: #3b82f6;
+  }
+`;
+
+const InputWrapper = styled.div<{ $styleString: string }>`
+  ${(props) => props.$styleString}
+`;
+
+export { InputContainer, Input, InputWrapper };
